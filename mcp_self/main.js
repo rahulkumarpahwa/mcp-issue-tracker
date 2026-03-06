b9b2008d88f86d5bc02a1d8491ac8703fd28c5a9
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2026-03-06 07:00:23 +0530

    till lecture 12 : Add Resource to the Claude Desktop

--- mcp_self/main.js
+++ mcp_self/main.js
@@ -1,16 +1,15 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import sqlite3 from "sqlite3";
-import path, { resolve } from "path";
+import path from "path";
 import { fileURLToPath } from "url";
-import { rejects } from "assert";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const server = new McpServer({
   name: "issue-server",
-  version: "0.0.1",
+  version: "1.0.0",
 });
 
 server.registerResource(
@@ -34,7 +33,7 @@ server.registerResource(
           if (err) {
             reject(err);
           } else {
-            resolve(rows.map((row) => row.sql + ";")).join("\n ");
+            resolve(rows.map((row) => row.sql + ";").join("\n "));
           }
         },
       );
@@ -54,3 +53,9 @@ server.registerResource(
 
 const transport = new StdioServerTransport();
 await server.connect(transport);
+
+// when we attach a resource or a big file, we are attaching it as the resource. here the file act as the resource we attach to the claude and we will get the data accessed in it.
+// similar cases would be when we attach the google drive and get accessed to the pdf or any docs or sheet to get our answers out of that.
+
+// Now, we will ask the questions to the claude like, "Explain me the schema in plain english for my github style issue tracker"
+// so, we can add the resources ourself we we want to ask the questions or understand that.
